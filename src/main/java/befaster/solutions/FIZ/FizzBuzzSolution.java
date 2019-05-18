bde59a3c415b5ac7c4609ce6303e44ed7f41c794
sbx_user1051 <sbx_user1051@169.254.131.97>
2019-05-18 11:45:52 +0000

    Sat May 18 11:45:52 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -10,11 +10,11 @@ public class FizzBuzzSolution {
         String comparisonValue = "";
         comparisonValue = comparisonValue + number;
 
-        if ( (number % 3 == 0) && comparisonValue.contains("3") ) {
+        if ( (number % 3 == 0) || comparisonValue.contains("3") ) {
             result = result + "fizz";
         }
 
-        if ( (number % 5 == 0) && comparisonValue.contains("5") ) {
+        if ( (number % 5 == 0) || comparisonValue.contains("5") ) {
             if ( result == "" ) {
                 result = result + "buzz";
             } else {
@@ -25,15 +25,16 @@ public class FizzBuzzSolution {
         }
 
 
-        if ( ( result != "" ) && ( number % 2 != 0 ) ) {
-            return result + " fake deluxe";
+        if ( ((number % 3 == 0) && comparisonValue.contains("3")) || ((number % 5 == 0) && comparisonValue.contains("5")) ) {
+            if ((result != "") && (number % 2 != 0)) {
+                return result + " fake deluxe";
+            }
+
+            if ((result != "")) {
+                return result + " deluxe";
+            }
+
         }
-
-        if ( ( result != "" ) ) {
-            return result + " deluxe";
-        }
-
-
 
 
         if ( result == "" )
@@ -44,5 +45,6 @@ public class FizzBuzzSolution {
         return result;
     }
 }
+
 
 
